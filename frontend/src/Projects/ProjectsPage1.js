77b81f9277c75931f180ced1c5461f0ce1a72715
Nikhil Kohli <nikhilkohli2407@gmail.com>
2025-02-16 11:03:31 +0530

    updates

--- frontend/src/Projects/ProjectsPage1.js
+++ frontend/src/Projects/ProjectsPage1.js
@@ -13,7 +13,7 @@ const ProjectsPage1 = () => {
           <li><img src={Linkedin}/><span><a className={Style.links} href="https://github.com/nikhilko-07/LinkedinClone"><h3>Linkedin Clone Project</h3><p>click to go github page</p></a></span></li>
           <li><img src={Soil}/><span><a className={Style.links} href="https://github.com/nikhilko-07/Soil-Agriculture"><h3>Soil info Project</h3><p>click to go github page</p></a></span></li>
           <li><img src={VideoChat}/><span><a className={Style.links} href="https://github.com/nikhilko-07/LOOP-Video-Meet"><h3>Zoom Clone Project</h3><p>click to go github page</p></a></span></li>
-          <li><img src={MemoryGame}/><span><a href="https://github.com/nikhilko-07/Memory-Card-Game" className={Style.links} ><h3>Linkedin Clone</h3><p>click to go github page</p></a></span></li>
+          <li><img src={MemoryGame}/><span><a href="https://github.com/nikhilko-07/Memory-Card-Game" className={Style.links} ><h3>Memory Card Game</h3><p>click to go github page</p></a></span></li>
       </div>
     </div>
   )
