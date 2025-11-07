2bd5b3bea919ccb0222ac0e2d3f0e3b7a8877ada
nikhilko-07 <nikhilkohli2407@gmail.com>
2025-11-07 22:18:13 +0530

    Some Push

--- frontend/src/Projects/ProjectsPage1.js
+++ frontend/src/Projects/ProjectsPage1.js
@@ -4,14 +4,15 @@ import Linkedin from "./Linkedin.png";
 import Soil from "./Soil.png";
 import VideoChat from "./VideoChat.png";
 import Cater from "./cater.png";
+import Node from "./NodeMailer.png";
 
 const ProjectsPage1 = () => {
   return (
     <div className={`${Style.container} container`}>
       <h1 className={Style.header}>My Recent Works</h1>
       <div className={Style.list}>
-          <li><img src={Linkedin}/><span><a className={Style.links} href="https://github.com/nikhilko-07/LinkedinClone"><h3>Linkedin Clone Project</h3><p>click to go github page</p></a></span></li>
-          <li><img src={Soil}/><span><a className={Style.links} href="https://github.com/nikhilko-07/Soil-Agriculture"><h3>Soil info Project</h3><p>click to go github page</p></a></span></li>
+          <li><img src={Linkedin}/><span><a className={Style.links} href="https://linkedin-clone-sandy-eight.vercel.app"><h3>Linkedin Clone Project</h3><p>click to go github page</p></a></span></li>
+          <li><img src={Node}/><span><a className={Style.links} href="https://gmail.nikhil.my"><h3>Soil info Project</h3><p>click to go github page</p></a></span></li>
           <li><img src={VideoChat}/><span><a className={Style.links} href="https://github.com/nikhilko-07/LOOP-Video-Meet"><h3>Zoom Clone Project</h3><p>click to go github page</p></a></span></li>
           <li><img src={Cater}/><span><a href="https://github.com/nikhilko-07/Catering-App" className={Style.links} ><h3>Catering Order Project</h3><p>click to go github page</p></a></span></li>
       </div>
