092731c740471e6a72bb68c45d59bcf77c41a018
nikhilko-07 <nikhilkohli2407@gmail.com>
2025-11-07 22:23:00 +0530

    updates

--- frontend/src/Projects/ProjectsPage1.js
+++ frontend/src/Projects/ProjectsPage1.js
@@ -11,8 +11,8 @@ const ProjectsPage1 = () => {
     <div className={`${Style.container} container`}>
       <h1 className={Style.header}>My Recent Works</h1>
       <div className={Style.list}>
-          <li><img src={Linkedin}/><span><a className={Style.links} href="https://linkedin-clone-sandy-eight.vercel.app"><h3>Linkedin Clone Project</h3><p>click to go github page</p></a></span></li>
-          <li><img src={Node}/><span><a className={Style.links} href="https://gmail.nikhil.my"><h3>Soil info Project</h3><p>click to go github page</p></a></span></li>
+          <li><img src={Linkedin}/><span><a className={Style.links} href="https://linkedin-clone-sandy-eight.vercel.app"><h3>Linkedin Clone Project</h3><p>click to go Live page</p></a></span></li>
+          <li><img src={Node}/><span><a className={Style.links} href="https://gmail.nikhil.my"><h3>NodeMailer Project</h3><p>click to go Live page</p></a></span></li>
           <li><img src={VideoChat}/><span><a className={Style.links} href="https://github.com/nikhilko-07/LOOP-Video-Meet"><h3>Zoom Clone Project</h3><p>click to go github page</p></a></span></li>
           <li><img src={Cater}/><span><a href="https://github.com/nikhilko-07/Catering-App" className={Style.links} ><h3>Catering Order Project</h3><p>click to go github page</p></a></span></li>
       </div>
